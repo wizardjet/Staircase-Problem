e5e458e07edf791cfa8acf48faded5ffa85fba0c
wizardjet <cx.jet97@yahoo.co.uk>
2018-02-01 15:29:09 +0000

    fixed minor bug

--- Solution.java
+++ Solution.java
@@ -1,6 +1,7 @@
 public class Solution {
 
     private static final int MAX_SUM = 5;
+    private static final int MAX_STEPS = 3;
 
     private static int numberOfWays = 0;
 
@@ -8,7 +9,7 @@ public class Solution {
         if (sum == MAX_SUM) {
             numberOfWays++;
         } else if (sum < MAX_SUM) {
-            for (int i = 1; i < MAX_SUM; i++) {
+            for (int i = 1; i < MAX_STEPS; i++) {
                 recurse(sum + i);
             }
         }
